edcf3c55b386fcc6ff7d6b5ca0d643d1d8717311
Bar Stromza <bstromza@bstromza-ws.tlv.corp.google.com>
2017-06-13 19:53:28 +0300

    clock start

--- extension.js
+++ extension.js
@@ -9,19 +9,36 @@ function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "lazy-toggle" is now active!');
-
+    
+    
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    var disposable = vscode.commands.registerCommand('extension.initClock', () => {
         // The code you place here will be executed every time your command is executed
+        this.showStatusBar = function() {
+            this.statusBarItem.text = `$(triangle-right)  Start Clock`;
+            this.statusBarItem.command = 'extension.startClock';
+            this.statusBarItem.show();
+        }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        this.statusBarItem.text = `$(triangle-right)  yokev!`;
+        this.showStatusBar();
+    });
+
+        // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerCommand('extension.startClock', () => {
+        // The code you place here will be executed every time your command is executed
+        // this.statusBarItem.command = 'extension.stopClock';
+        this.statusBarItem.text = `$(clock)  Stop Clock`;
     });
 
     context.subscriptions.push(disposable);
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
